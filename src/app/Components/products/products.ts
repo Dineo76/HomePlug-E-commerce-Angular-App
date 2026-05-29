24321783964dce218966ea274e9abf07fc0b0ddc
Dineo <Dineo.Makofane@ext.shaper.co.za>
2026-05-29 10:47:38 +0200

    merging Develop branch to Products branch

--- src/app/Components/products/products.ts
+++ src/app/Components/products/products.ts
@@ -1,9 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-products',
-  imports: [],
-  templateUrl: './products.html',
-  styleUrl: './products.css',
-})
-export class Products {}

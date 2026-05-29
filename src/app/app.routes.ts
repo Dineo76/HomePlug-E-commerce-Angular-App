057b3a1fcfd72b987df9e15d1f46ac64a93ab9fe
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-05-29 11:36:19 +0200

    feature: Authentication setup (logout)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './Components/register/register';
 
+import { LoginComponent } from './Components/login/login';
+
+
 export const routes: Routes = [
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
 ];
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { AuthService } from '../services/auth';
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+   this.authService.logout()
+
+      .then(() => {
+
+        alert('Logged out');
+
+        this.router.navigate(['/login']);
+
+      })
+
+      .catch((error) => {
+
+        console.error(error);
+
+      });
+  }
+}

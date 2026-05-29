b02aee0c5b4eb73312b0f815de723534527119f6
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-05-29 15:58:41 +0200

    UI:forgot password page ui design

--- src/app/Components/forgot-password/forgot-password.ts
+++ src/app/Components/forgot-password/forgot-password.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {RouterLink } from '@angular/router';
 
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './forgot-password.html',
   styleUrls: ['./forgot-password.css']
 })

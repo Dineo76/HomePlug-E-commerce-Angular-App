a79802aaed3a2b52a70e8f89685e77fb1373cf38
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-05-29 15:51:57 +0200

    UI:register page ui design

--- src/app/Components/register/register.ts
+++ src/app/Components/register/register.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
@@ -14,6 +15,8 @@ export class RegisterComponent {
 
   email = '';
   password = '';
+  firstName = '';
+  lastName = '';
 
   constructor(private authService: AuthService) {}
 

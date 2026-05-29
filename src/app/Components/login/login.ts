08f41a7feb78a559df8e0708001326aed6283220
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-05-29 11:00:22 +0200

    feature: Authentication setup (login)

--- src/app/Components/login/login.ts
+++ src/app/Components/login/login.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css']
 })
-export class Login {}
+export class LoginComponent {
+
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+
+    this.authService
+      .login(this.email, this.password)
+
+      .then((userCredential) => {
+
+        console.log('Logged in:', userCredential.user);
+
+        alert('Login successful');
+
+      })
+
+      .catch((error) => {
+
+        console.error(error);
+
+        alert(error.message);
+
+      });
+  }
+}

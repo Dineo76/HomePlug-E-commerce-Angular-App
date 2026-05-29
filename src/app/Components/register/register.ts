d30eb41fbd18b2ff55884d0d675e9cc7ca06f93e
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-05-29 10:33:06 +0200

    feature: Authentication setup (register)

--- src/app/Components/register/register.ts
+++ src/app/Components/register/register.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrls: ['./register.css']
 })
-export class Register {}
+export class RegisterComponent {
+
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService) {}
+
+  register() {
+
+    this.authService
+      .register(this.email, this.password)
+
+      .then(() => {
+        alert('Registration successful');
+      })
+
+      .catch((error) => {
+        alert(error.message);
+      });
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+// Firebase configuration
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDt9oIV6viANu8rfijr60f2K9aYL3JNaaY",
+    authDomain: "homeplug-34c40.firebaseapp.com",
+    projectId: "homeplug-34c40",
+    storageBucket: "homeplug-34c40.firebasestorage.app",
+    messagingSenderId: "936565304217",
+    appId: "1:936565304217:web:385955c94757b527a78108",
+    measurementId: "G-PT27LBMDBV"
+  }
+};

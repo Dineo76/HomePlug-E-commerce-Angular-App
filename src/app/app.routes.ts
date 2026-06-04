bfb4d54827d3df85316511a74469eceb8e9a8318
Nonjie Phoswa <nonjie.phoswa@gmail.com>
2026-06-04 08:33:25 +0200

    ui update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { ForgotPasswordComponent } from './Components/forgot-password/forgot-pas
 import { LoginComponent } from './Components/login/login';
 import { RegisterComponent } from './Components/register/register';
 import { CheckoutComponent } from './Components/checkout/checkout';
+import { FAQs } from './Components/faqs/faqs';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'products', component: Products },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'faqs', component: FAQs },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'checkout', component: CheckoutComponent },
